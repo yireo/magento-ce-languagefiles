d4c7d070937c3628155def81d4d9ec571a6ffa41
Yireo <info@yireo.com>
2013-08-13 17:54:25 +0200

    Add package.sh to run.sh to allow for dead-simple cronjob

--- update/run.sh
+++ update/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 php ./update.php
+./package.sh
 git add ../magento-ce-17
+git add ../packages
 git commit -m "Updated language-files from Transifex" ../magento-ce-17
 git push origin master
