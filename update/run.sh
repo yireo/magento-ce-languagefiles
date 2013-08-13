547ad97c3f4906f1dfd6010bedea40d299b905a8
Yireo <info@yireo.com>
2013-08-13 18:01:56 +0200

    Autocommit ZIP-packages to GitHub

--- update/run.sh
+++ update/run.sh
@@ -4,4 +4,5 @@ php ./update.php
 git add ../magento-ce-17
 git add ../packages
 git commit -m "Updated language-files from Transifex" ../magento-ce-17
+git commit -m "Updated packages from Transifex" ../packages
 git push origin master
